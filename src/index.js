7fdaa731716287cdf88bd1b924cf529970eaefab
Michael Martinez <martinez1212@gmail.com>
2017-04-04 16:59:47 -0700

    added bootstrap filter component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import buttonGroupInstance from './components/filterButtonGroup'
+
 
 ReactDOM.render(
   <App />,
+  // <buttonGroupInstance />,
   document.getElementById('root')
 );
