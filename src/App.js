bab7a97dbf621a8fbe469a6214804570b25bdbbf
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-09 10:19:06 -0700

    added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import buttonGroupInstance from './components/filterButtonGroup'
 import navbarInstance from './components/navBar'
+import tableInstance from './components/cheese_address'
 
 class App extends Component {
   render() {
@@ -11,10 +12,9 @@ class App extends Component {
         <div className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           {navbarInstance}
-
           <h2>Welcome to React</h2>
           {buttonGroupInstance}
-
+          {tableInstance}
         </div>
 
         <p className="App-intro">
