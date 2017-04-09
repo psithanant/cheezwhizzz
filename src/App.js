6c7d1d36fd10a1536b87593bea808f3ce0d9950b
Aom <p.sithanant@gmail.com>
2017-04-09 10:15:25 -0700

    Filtered Table: Added input form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, {Component} from 'react';
-
 import buttonGroupInstance from './components/filterButtonGroup'
 import NavbarClass from './components/navBar/navBar'
-
 import { Grid, Col } from 'react-bootstrap';
-
 import './App.css';
 import Menu from './components/filterButtonGroup'
-
 import ResultTable from './components/resultTable'
-
 
 const fakeCheeseData = [
   {name: "brie", hardness: "soft", animal: "cow", favorite: 0, id: 1},
 ];
-
 
 class App extends Component {
   render() {
