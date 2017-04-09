67d702f9237d3f8b383ed215a76dbdd18f551345
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-09 12:45:10 -0700

    added new change

--- src/App.js
+++ src/App.js
@@ -7,15 +7,10 @@ import {
 } from 'react-router-dom'
 
 import buttonGroupInstance from './components/filterButtonGroup'
-<<<<<<< HEAD
-import navbarInstance from './components/navBar'
-import tableInstance from './components/cheese_address'
-=======
 import { Grid, Col } from 'react-bootstrap';
 import Menu from './components/filterButtonGroup'
 import NavbarClass from './components/navBar/navBar'
 import ResultTable from './components/resultTable'
->>>>>>> 885bb55d73d964a61302b27a1500f24ba69bab9f
 
 
 const fakeCheeseData = [
@@ -28,16 +23,6 @@ const fakeCheeseData = [
 class FindStore extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <div className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {navbarInstance}
-          <h2>Welcome to React</h2>
-          {buttonGroupInstance}
-          {tableInstance}
-        </div>
-=======
       <div className="FindStore">
         <p>hiiii</p>
       </div>
@@ -68,7 +53,6 @@ class Home extends Component {
     );
   }
 }
->>>>>>> 885bb55d73d964a61302b27a1500f24ba69bab9f
 
 class App extends Component {
   ////////// Render
