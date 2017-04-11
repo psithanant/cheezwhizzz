6e5a3778a5b7c97a6aaa49debafac7fad6eb7d63
Aom <p.sithanant@gmail.com>
2017-04-10 20:34:14 -0700

    Reformatted code for resultTable

--- src/components/homepage/resultTable.js
+++ src/components/homepage/resultTable.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-const ResultTable = ({cheeses}) => {
-    let rows = [];
-    cheeses.forEach(function(cheese) {
-        rows.push(
-            <tr key={cheese.id}>
-                <td>
-                    {cheese.name}
-                </td>
-                <td>
-                    {cheese.firmness}
-                </td>
-                <td>
-                    {cheese.animal}
-                </td>
-                <td>
-                    <input type="checkbox"/>
-                </td>
+const ResultTable = ({cheeses}) => (
+    <table className='table-striped table-bordered table'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Firmness</th>
+                <th>Animal</th>
+                <th>Favorite</th>
             </tr>
-        )
-    });
-    return (
-        <table className='table-striped table-bordered table'>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Firmness</th>
-                    <th>Animal</th>
-                    <th>Favorite</th>
-                </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </table>
-    )
-}
+        </thead>
+        <tbody>
+        {cheeses.map((cheese) => (
+          <tr key={cheese.id}>
+              <td>{cheese.name}</td>
+              <td>{cheese.firmness}</td>
+              <td>{cheese.animal}</td>
+              <td><input type="checkbox"/></td>
+          </tr>
+        ))}
+        </tbody>
+    </table>
+)
 
 export default ResultTable;
