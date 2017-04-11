d0895c948f5b0cd40516a65aa9eed2adef66082e
Michael Martinez <martinez1212@gmail.com>
2017-04-11 13:32:50 -0700

    cleaned up code

--- src/components/navBar/navBarResultstable.js
+++ src/components/navBar/navBarResultstable.js
@@ -1,4 +1,3 @@
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
@@ -9,29 +8,10 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-// const data = [];
-// class NavTable extends Component {
-//   render() {
-//     console.log('this is list', this.props.list);
-//     return (
-//       <BootstrapTable data={ this.props.list }>
-//         <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
-//         <TableHeaderColumn dataField='animal' isKey>Animal</TableHeaderColumn>
-//         <TableHeaderColumn dataField='firmness'>Firmness</TableHeaderColumn>
-//       </BootstrapTable>
-//     );
-//   }
-// }
-
-// export default NavTable;
-
-
 class NavTable extends Component {
 
     render() {
-      // console.log('ok', this.props.list)
         let rows = [];
-        console.log('this is propsssss', this.props.list);
          this.props.list.forEach(function(cheese) {
             rows.push(
                 <tr key={cheese.id}>
@@ -63,42 +43,6 @@ class NavTable extends Component {
         )
     }
 }
-
-// const _renderList = list => (
-//   list
-//     .map(cheese => (
-//       <tr key={cheese.id}>
-//         <td>
-//           {cheese.name}
-//         </td>
-//         <td>
-//           {cheese.firmness}
-//         </td>
-//         <td>
-//           {cheese.animal}
-//         </td>
-//       </tr>
-//     ))
-// );
-//
-// const NavTable = ({ list, page }) => {
-//   return (
-//     <table className="table">
-//       <thead>
-//         <tr>
-//           <th>Name</th>
-//         <th>Firmness</th>
-//         <th>Animal</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {console.log('this is list', list)}
-//
-//         {_renderList(list)}
-//       </tbody>
-//     </table>
-//   );
-// };
 
 
 export default connect(mapStateToProps)(NavTable)
